c513b9b280aff138d92fb8b211352307ec7c30eb
Alexander Gramovich <34745780+ggramal@users.noreply.github.com>
2024-03-26 18:19:10 +0300

    gcp: vm module (#6)
    * gcp: vm module
    
    * pd-balanced as default storage type for vm

---
+++ modules/gcp/vm/input.tf
@@ -0,0 +1,112 @@
+variable "name" {
+  description = "vm name"
+  type        = string
+}
+
+variable "project_id" {
+  description = "gcp project id"
+  type        = string
+}
+
+variable "machine_type" {
+  description = "vm machine type https://cloud.google.com/compute/docs/machine-resource"
+  type        = string
+}
+
+variable "disk" {
+  description = "vm disk definition"
+  type = object(
+    {
+      size_gb = number,
+      type    = string
+    }
+  )
+  default = {
+    size_gb = 10,
+    type    = "pd-balanced",
+  }
+}
+
+variable "network_config" {
+  description = "Network config of the vm"
+  type = object(
+    {
+      subnet = string
+      public_ip = optional(
+        object(
+          {
+            static = optional(
+              object(
+                {
+                  name        = string
+                  description = optional(string, "")
+                }
+              ),
+            )
+            ephemeral = optional(bool, false)
+            #One of PREMIUM, STANDARD or FIXED_STANDARD.
+            network_tier = optional(string, "STANDARD")
+          }
+        )
+      )
+    }
+  )
+}
+
+variable "zone" {
+  description = "zone where the vm should run"
+  type        = string
+}
+
+variable "image" {
+  description = "vm image definition"
+  type = object(
+    {
+      name    = optional(string, "")
+      project = optional(string)
+      family  = optional(string)
+    }
+  )
+}
+
+variable "metadata" {
+  description = "vm metadata"
+  type        = map(string)
+  default     = {}
+}
+
+variable "service_account" {
+  description = "Service account definition"
+  type = object(
+    {
+      name = optional(string),
+      scopes = optional(
+        list(string),
+        []
+      ),
+      email       = optional(string),
+      description = optional(string, "")
+      roles = optional(
+        list(string)
+      )
+      sa_iam_bindings = optional(
+        map(
+          list(string)
+        ),
+        {}
+      )
+    }
+  )
+}
+
+variable "tags" {
+  description = "vm tags"
+  type        = list(string)
+  default     = []
+}
+
+variable "labels" {
+  description = "vm lables"
+  type        = map(string)
+  default     = {}
+}
---
+++ modules/gcp/vm/main.tf
@@ -0,0 +1,58 @@
+locals {
+  public_ip        = var.network_config.public_ip
+  create_static_ip = local.public_ip == null ? false : !local.public_ip.ephemeral
+}
+
+module "template" {
+  source = "git::https://git@github.com/terraform-google-modules/terraform-google-vm.git//modules/instance_template?ref=v11.1.0"
+
+  machine_type = var.machine_type
+  disk_size_gb = tostring(var.disk.size_gb)
+  disk_type    = var.disk.type
+  subnetwork   = var.network_config.subnet
+  service_account = {
+    email  = var.service_account.email != null ? var.service_account.email : module.service_account[0].service_account.email
+    scopes = var.service_account.scopes
+  }
+  source_image         = var.image.name
+  source_image_project = var.image.project
+  source_image_family  = var.image.family
+  metadata             = var.metadata
+
+  tags = var.tags
+}
+
+module "service_account" {
+  source          = "../iam/service_account"
+  count           = var.service_account.email != null ? 0 : 1
+  name            = var.service_account.name
+  project_id      = var.project_id
+  description     = var.service_account.description
+  roles           = var.service_account.roles
+  sa_iam_bindings = var.service_account.sa_iam_bindings
+  generate_key    = false
+}
+
+resource "google_compute_instance_from_template" "this" {
+  name                     = var.name
+  zone                     = var.zone
+  source_instance_template = module.template.self_link
+  network_interface {
+    subnetwork = var.network_config.subnet
+    access_config = local.public_ip == null ? [] : [
+      {
+        nat_ip                 = local.public_ip.ephemeral == true ? null : google_compute_address.public_ip[local.public_ip.static.name].address
+        network_tier           = local.public_ip.network_tier
+        public_ptr_domain_name = ""
+      }
+    ]
+  }
+}
+
+resource "google_compute_address" "public_ip" {
+  for_each = local.create_static_ip == false ? {} : {
+    "${local.public_ip.static.name}" = local.public_ip.static
+  }
+  name        = each.value.name
+  description = each.value.description
+}
