05598b2f7ddc055b45f8e9e089cae391f6cb8711
St.Dmitry <hector@defigang.dev>
2024-05-10 16:31:58 +0000

    code refactor

--- modules/gcp/pubsub/iam/main.tf
+++ modules/gcp/pubsub/iam/main.tf
@@ -1,61 +1,61 @@
 resource "google_pubsub_subscription_iam_binding" "admin" {
-  count        = length(var.admins) > 0 && var.is_topic != true ? 1 : 0
+  count        = length(var.admins) > 0 && !var.is_topic ? 1 : 0
   subscription = var.name
   role         = "roles/pubsub.admin"
   members      = var.admins
 }
 
 resource "google_pubsub_subscription_iam_binding" "viewer" {
-  count        = length(var.viewers) > 0 && var.is_topic != true ? 1 : 0
+  count        = length(var.viewers) > 0 && !var.is_topic ? 1 : 0
   subscription = var.name
   role         = "roles/pubsub.viewer"
   members      = var.viewers
 }
 
 resource "google_pubsub_subscription_iam_binding" "subscriber" {
-  count        = length(var.subscribers) > 0 && var.is_topic != true ? 1 : 0
+  count        = length(var.subscribers) > 0 && !var.is_topic ? 1 : 0
   subscription = var.name
   role         = "roles/pubsub.subscriber"
   members      = var.subscribers
 }
 
 resource "google_pubsub_subscription_iam_binding" "editor" {
-  count        = length(var.editors) > 0 && var.is_topic != true ? 1 : 0
+  count        = length(var.editors) > 0 && !var.is_topic ? 1 : 0
   subscription = var.name
   role         = "roles/pubsub.editor"
   members      = var.editors
 }
 
 resource "google_pubsub_topic_iam_binding" "publisher" {
-  count   = length(var.publishers) > 0 && var.is_topic == true ? 1 : 0
+  count   = length(var.publishers) > 0 && var.is_topic ? 1 : 0
   topic   = var.name
   role    = "roles/pubsub.publisher"
   members = var.publishers
 }
 
 resource "google_pubsub_topic_iam_binding" "subscriber" {
-  count   = length(var.subscribers) > 0 && var.is_topic == true ? 1 : 0
+  count   = length(var.subscribers) > 0 && var.is_topic ? 1 : 0
   topic   = var.name
   role    = "roles/pubsub.subscriber"
   members = var.subscribers
 }
 
 resource "google_pubsub_topic_iam_binding" "editor" {
-  count   = length(var.editors) > 0 && var.is_topic == true ? 1 : 0
+  count   = length(var.editors) > 0 && var.is_topic ? 1 : 0
   topic   = var.name
   role    = "roles/pubsub.editor"
   members = var.editors
 }
 
 resource "google_pubsub_topic_iam_binding" "admin" {
-  count   = length(var.admins) > 0 && var.is_topic == true ? 1 : 0
+  count   = length(var.admins) > 0 && var.is_topic ? 1 : 0
   topic   = var.name
   role    = "roles/pubsub.admin"
   members = var.admins
 }
 
 resource "google_pubsub_topic_iam_binding" "viewer" {
-  count   = length(var.viewers) > 0 && var.is_topic == true ? 1 : 0
+  count   = length(var.viewers) > 0 && var.is_topic ? 1 : 0
   topic   = var.name
   role    = "roles/pubsub.viewer"
   members = var.viewers
