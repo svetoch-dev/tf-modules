6eaa49041572bd3c0d45a10e43fee4e1c74aecfe
Ruslan Palikarpau <r.palikarpau@trykitchenhub.com>
2024-10-01 16:52:40 +0300

    del outputs

--- modules/gcp/monitoring/log_metrics/output.tf
+++
@@ -1,9 +0,0 @@
-output "logging_metric_name" {
-  description = "name metric"
-  value       = google_logging_metric.logging_metric.name
-}
-
-output "logging_metric_filter" {
-  description = "metric filter"
-  value       = google_logging_metric.logging_metric.filter
-}
--- modules/gcp/monitoring/output.tf
+++
@@ -1,4 +0,0 @@
-output "log_metric_names" {
-  description = "metrics names"
-  value       = module.log_metrics[*].logging_metric_name
-}
