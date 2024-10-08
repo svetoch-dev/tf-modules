657123f536539ead7a3b0621b170cb201cd007d2
Ruslan Palikarpau <r.palikarpau@trykitchenhub.com>
2024-10-08 16:01:11 +0300

    dtata

--- modules/gcp/monitoring/dashboards/main.tf
+++ modules/gcp/monitoring/dashboards/main.tf
@@ -1,7 +1,13 @@
-resource "google_monitoring_dashboard" "dashboard" {
-  dashboard_json = templatefile("${path.module}/templates/dashboard.json.tpl", {
+data "template_file" "dashboard_json" {
+  template = file("${path.module}/templates/dashboard.json.tpl")
+
+  vars = {
     display_name = var.display_name
     columns      = var.columns
     tiles        = var.tiles
-  })
+  }
+}
+
+resource "google_monitoring_dashboard" "dashboard" {
+  dashboard_json = data.template_file.dashboard_json.rendered
 }
