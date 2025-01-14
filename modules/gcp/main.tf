6b2158596320fed2b1dba659e653e2730cf6dbbf
Ruslan Palikarpau <r.palikarpau@trykitchenhub.com>
2025-01-14 14:58:09 +0300

    fix main.tf

--- modules/gcp/main.tf
+++ modules/gcp/main.tf
@@ -447,4 +447,5 @@ module "monitoring" {
   log_metrics           = try(var.monitoring.log_metrics, [])
   dashboards            = try(var.monitoring.dashboards, [])
   notification_channels = try(var.monitoring.notification_channels, [])
+  alert_policies        = try(var.monitoring.alert_policies, [])
 }
