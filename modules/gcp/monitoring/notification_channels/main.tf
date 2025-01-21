f3ad8d60808bdbb1a252465c545f339cc88ae0d7
Ruslan Palikarpau <r.palikarpau@trykitchenhub.com>
2025-01-21 15:18:08 +0300

    dynamic block

--- modules/gcp/monitoring/notification_channels/main.tf
+++ modules/gcp/monitoring/notification_channels/main.tf
@@ -3,9 +3,12 @@ resource "google_monitoring_notification_channel" "default" {
   type         = var.type
   labels       = var.labels
 
-  sensitive_labels {
-    auth_token  = var.sensitive_labels.auth_token
-    password    = var.sensitive_labels.password
-    service_key = var.sensitive_labels.service_key
+  dynamic "sensitive_labels" {
+    for_each = var.sensitive_labels != null ? [var.sensitive_labels] : []
+    content {
+      auth_token  = var.sensitive_labels.auth_token
+      password    = var.sensitive_labels.password
+      service_key = var.sensitive_labels.service_key
+    }
   }
 }
