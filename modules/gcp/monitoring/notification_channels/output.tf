99e958a5de228032e2ead4513ca6a439a061c4b9
Ruslan Palikarpau <r.palikarpau@trykitchenhub.com>
2025-01-21 14:10:20 +0300

    fix outputs in notification channels

--- modules/gcp/monitoring/notification_channels/output.tf
+++ modules/gcp/monitoring/notification_channels/output.tf
@@ -1,9 +1,9 @@
 output "notification_channel_id" {
   description = "The ID of the created notification channel."
-  value       = google_monitoring_notification_channel.notification_channel.id
+  value       = google_monitoring_notification_channel.default.id
 }
 
 output "notification_channel_name" {
   description = "The name of the created notification channel."
-  value       = google_monitoring_notification_channel.notification_channel.name
+  value       = google_monitoring_notification_channel.default.name
 }
