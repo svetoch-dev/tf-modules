3e3ccf4a96c046a062345a83f16234b05e71ed26
Gramovich Alexander <gramovich.alex@outlook.com>
2025-02-28 14:08:49 +0300

    github: change log update + version in repo module

--- CHANGELOG_GITHUB.md
+++ CHANGELOG_GITHUB.md
@@ -1,3 +1,9 @@
+# github-v0.2.0
+
+enhancements:
+* remove unused org attr of repositories var
+* version providers in module itself
+
 # github-v0.1.1
 
 fixes:
--- modules/github/repository/versions.tf
+++ modules/github/repository/versions.tf
@@ -1,7 +1,8 @@
 terraform {
   required_providers {
     github = {
-      source = "integrations/github"
+      source  = "integrations/github"
+      version = "6.4.0"
     }
   }
 }
