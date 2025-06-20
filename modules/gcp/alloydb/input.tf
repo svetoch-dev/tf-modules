c5ec04fac804e8cc98bba9c9fe8ee6286016ebc7
Gramovich Alexander <gramovich.alex@outlook.com>
2025-06-20 19:08:27 +0300

    alloydb: initial commit

---
+++ modules/gcp/alloydb/input.tf
@@ -0,0 +1,116 @@
+variable "name" {
+  description = "Alloydb cluster id"
+  type        = string
+}
+
+variable "region" {
+  description = "gcp region"
+  type        = string
+}
+
+variable "database_version" {
+  description = "postgresql version"
+  type        = string
+  default     = "POSTGRES_17"
+}
+
+variable "labels" {
+  description = "Alloydb cluster labels"
+  type        = map(string)
+  default     = {}
+}
+
+variable "annotations" {
+  description = "Alloydb cluster annotations"
+  type        = map(string)
+  default     = {}
+}
+
+variable "cluster_type" {
+  description = "Alloydb cluster type"
+  type        = string
+  default     = "PRIMARY"
+}
+
+variable "project_id" {
+  description = "gcp project id"
+  type        = string
+}
+
+variable "subscription_type" {
+  description = "Alloydb cluster subsription type TRIAL/STANDARD."
+  type        = string
+  default     = "STANDARD"
+}
+
+variable "deletion_policy" {
+  description = "Alloydb cluter deletion policy DEFAULT/FORCE"
+  type        = string
+  default     = "DEFAULT"
+}
+
+variable "encryption_config" {
+  description = "Alloydb cluster encryption config"
+  type = object(
+    {
+      kms_key = string
+    }
+  )
+  default = {}
+}
+
+variable "network_config" {
+  description = "Alloydb cluster network config"
+  type = object(
+    {
+      network            = string
+      allocated_ip_range = optional(string)
+    }
+  )
+  default = {}
+}
+
+variable "restore_continuous_backup_source" {
+  description = "Alloydb cluster restore configs using continuous backup"
+  type = object(
+    {
+      cluster       = string
+      point_in_time = string
+    }
+  )
+  default = {}
+}
+
+variable "continuous_backup_config" {
+  description = "Alloydb cluster continuous backup configs"
+  type = object(
+    {
+      enabled              = optional(bool, true)
+      recovery_window_days = optional(number, 14)
+      encryption_config = optional(
+        object(
+          {
+            kms_key = string
+          }
+        )
+      )
+    }
+  )
+  default = {
+    enabled = true
+  }
+}
+
+variable "users" {
+  description = "Alloydb cluster users"
+  type = map(
+    object(
+      {
+        name           = string
+        type           = optional(string, "ALLOYDB_BUILT_IN")
+        database_roles = optional(list(string))
+      }
+    )
+  )
+  default = {}
+}
---
+++ modules/gcp/alloydb/main.tf
@@ -0,0 +1,55 @@
+resource "google_alloydb_cluster" "main" {
+  cluster_id        = var.name
+  location          = var.region
+  database_version  = var.database_version
+  lables            = var.labels
+  annotations       = var.annotations
+  cluster_type      = var.cluster_type
+  project           = var.project_id
+  subscription_type = var.subscription_type
+  deletion_policy   = var.deletion_policy
+
+  dynamic "encryption_config" {
+    for_each = var.encryption_config
+    content {
+      name = encryption_config.value.kms_key
+    }
+  }
+
+  dynamic "network_config" {
+    for_each = var.network_config
+    content {
+      network            = network_config.value.network
+      allocated_ip_range = network_config.value.allocated_ip_range
+    }
+  }
+
+  dynamic "restore_continuous_backup_source" {
+    for_each = var.restore_continuous_backup_source
+    content {
+      cluster       = restore_continuous_backup_source.value.cluster
+      point_in_time = restore_continuous_backup_source.value.point_in_time
+    }
+  }
+
+  dynamic "network_config" {
+    for_each = var.network_config
+    content {
+      network            = network_config.value.network
+      allocated_ip_range = network_config.value.allocated_ip_range
+    }
+  }
+  dynamic "continuous_backup_config" {
+    for_each = var.continuous_backup_config
+    content {
+      enabled              = continuous_backup_config.value.enabled
+      recovery_window_days = continuous_backup_config.value.recovery_window_days
+      dynamic "encryption_config" {
+        for_each = continuous_backup_config.value.encryption_config
+        content {
+          kms_key = encryption_config.value.kmg_key
+        }
+      }
+    }
+  }
+}
