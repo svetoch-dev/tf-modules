ef0e4be43bef7572abf8b9e2db7c89fa4dc3c2de
Gramovich Alexander <gramovich.alex@outlook.com>
2025-07-29 18:44:50 +0300

    fix output

--- modules/gcp/output.tf
+++ modules/gcp/output.tf
@@ -38,16 +38,7 @@ output "iam" {
 }
 
 output "cloudsql_postgres" {
-  value = {
-    for pg_name, pg_obj in module.cloudsql_postgres :
-    pg_name => merge(
-      pg_obj,
-      {
-        superuser_password = random_password.cloudsql_passwords[pg_name].result
-        superuser_name     = var.cloudsql_postgres[pg_name].user.name
-      }
-    )
-  }
+  value = module.cloudsql_postgres
 }
 
 output "redis" {
