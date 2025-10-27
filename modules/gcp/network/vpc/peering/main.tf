bb5e6001ae6c9b0638d5f6c2e4f24199caa62df4
Gramovich Alexander <gramovich.alex@outlook.com>
2025-10-27 14:27:36 +0300

    fix typo

--- modules/gcp/network/vpc/peering/main.tf
+++ modules/gcp/network/vpc/peering/main.tf
@@ -1,12 +1,12 @@
 resource "google_compute_network_peering" "local_to_peer" {
   name         = var.name
   network      = var.network
-  peer_network = va.peer_network
+  peer_network = var.peer_network
 }
 
 resource "google_compute_network_peering" "peer_to_local" {
   count        = var.create_reverse_peering ? 1 : 0
   name         = "${var.name}-reverse"
   network      = var.network
-  peer_network = va.peer_network
+  peer_network = var.peer_network
 }
