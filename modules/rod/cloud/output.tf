fb613ec03205c373c0c6fea81d480b107a744ae3
Gramovich Alexander <gramovich.alex@outlook.com>
2026-02-10 10:31:25 +0300

    proper vpc based network output

--- modules/rod/cloud/output.tf
+++ modules/rod/cloud/output.tf
@@ -1,10 +1,13 @@
 output "network" {
   value = merge(
     var.env.cloud.name == "gcp" ? {
-      vpcs            = module.gcp["this"].vpcs
-      subnets         = module.gcp["this"].subnets
-      nats            = module.gcp["this"].nats
-      service_peering = module.gcp["this"].service_peering
+      for network_name, vpc_obj in module.gcp["this"].vpcs :
+      network_name => {
+        vpcs            = module.gcp["this"].vpcs[network_name]
+        subnets         = module.gcp["this"].subnets[network_name]
+        nats            = module.gcp["this"].nats[network_name]
+        service_peering = module.gcp["this"].service_peering[network_name]
+      }
     } : {},
   )
 }
