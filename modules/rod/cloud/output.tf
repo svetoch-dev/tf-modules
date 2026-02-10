5dc7351578d9ea1a34356a6c246dcb4d8b4d13a8
Gramovich Alexander <gramovich.alex@outlook.com>
2026-02-10 10:15:59 +0300

    network outputs instead of vpcs

--- modules/rod/cloud/output.tf
+++ modules/rod/cloud/output.tf
@@ -1,5 +1,10 @@
-output "vpcs" {
+output "network" {
   value = merge(
-    var.env.cloud.name == "gcp" ? module.gcp["this"].vpcs : {},
+    var.env.cloud.name == "gcp" ? {
+      vpcs            = module.gcp["this"].vpcs
+      subnets         = module.gcp["this"].subnets
+      nats            = module.gcp["this"].nats
+      service_peering = module.gcp["this"].service_peering
+    } : {},
   )
 }
