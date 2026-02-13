c0f062010699789dcabbc0c4109445a2864452f8
Dzmitry Staliarou <leshiy74@gmail.com>
2026-02-13 11:04:06 +0300

    set using variables in tf_variables.tf

--- modules/rod/repo/tf_variables.tf
+++ modules/rod/repo/tf_variables.tf
@@ -13,94 +13,21 @@ variable "ci" {
   type = object(
     {
       type = string
+      group = string
     }
   )
   default = null
 }
 
-variable "apps" {
-  description = "Application related info"
-  type = map(
-    object(
-      {
-        name = string
-      }
-    )
-  )
-  default = {}
-}
-
-variable "env" {
-  description = "Environment description"
-  type = object(
-    {
-      name          = string
-      short_name    = string
-      initial_start = optional(bool, false)
-      cloud = object(
-        {
-          name = string
-          id   = string
-          location = object(
-            {
-              region       = string
-              default_zone = string
-              multi_region = string
-            }
-          )
-          network = object(
-            {
-              vm_cidr          = string
-              k8s_pod_cidr     = string
-              k8s_service_cidr = string
-            }
-          )
-          buckets = object(
-            {
-              deletion_protection = bool
-            }
-          )
-        }
-      )
-      kubernetes = optional(
-        object(
-          {
-            enabled             = bool
-            regional            = bool
-            deletion_protection = optional(bool, true)
-            location            = string
-            auth_group          = optional(string, "")
-          }
-        )
-      )
-    }
-  )
-}
 
 variable "overrides" {
-  description = "Cloud attribute overrides"
+  description = "Repositories attribute overrides"
   type = object(
     {
-      gcp_activate_apis     = optional(any)
-      gcp_buckets           = optional(any)
-      gcp_dns_zones         = optional(any)
-      gcp_iam               = optional(any)
-      gcp_k8s_clusters      = optional(any)
-      gcp_logging           = optional(any)
-      gcp_networks          = optional(any)
-      gcp_registries        = optional(any)
-      gcp_k8s_cluster_nodes = optional(any)
+      repositories = optional(any)
     }
   )
   default = {
-    gcp_activate_apis     = null
-    gcp_buckets           = null
-    gcp_dns_zones         = null
-    gcp_iam               = null
-    gcp_k8s_clusters      = null
-    gcp_k8s_cluster_nodes = null
-    gcp_logging           = null
-    gcp_networks          = null
-    gcp_registries        = null
+    repositories = null
   }
 }
