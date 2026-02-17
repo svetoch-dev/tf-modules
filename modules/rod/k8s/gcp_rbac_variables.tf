3e268faf87a61a06c8f2501fa73f9451ff94568a
Gramovich Alexander <gramovich.alex@outlook.com>
2026-02-17 10:32:31 +0300

    fix rbac_main var naming

--- modules/rod/k8s/gcp_rbac_variables.tf
+++ modules/rod/k8s/gcp_rbac_variables.tf
@@ -1,7 +1,7 @@
 locals {
   rbac_gcp = {
     service_accounts = {
-      for service_account_name, service_account_obj in local.rbac.service_accounts :
+      for service_account_name, service_account_obj in local.rbac_main.service_accounts :
       service_account_name => merge(
         service_account_obj,
         {
