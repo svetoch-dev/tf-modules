8f47261d3ba169e2e40490681e6674f939c57080
Gramovich Alexander <gramovich.alex@outlook.com>
2026-02-17 10:25:39 +0300

    k8s: update provider for all submodules

--- modules/k8s/rbac/versions.tf
+++ modules/k8s/rbac/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "2.36.0"
+      version = "2.38.0"
     }
   }
   required_version = ">= 0.13"
