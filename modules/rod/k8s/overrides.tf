41859d3052247b0f36dfadb9c1697d29b8c399fb
Gramovich Alexander <gramovich.alex@outlook.com>
2026-02-17 10:36:26 +0300

    rod/k8s fix var naming p3

--- modules/rod/k8s/overrides.tf
+++ modules/rod/k8s/overrides.tf
@@ -3,7 +3,7 @@ locals {
     {
       gcp = provider::deepmerge::mergo(local.rbac_main, local.rbac_gcp)
     },
-    var.env.name,
+    var.env.cloud.name,
     null
   )
   rbac_merged       = provider::deepmerge::mergo(local.rbac, var.overrides.rbac)
