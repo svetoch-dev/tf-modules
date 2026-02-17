4aaa21fe24b63f1003a35845b3a61b2563c15398
Gramovich Alexander <gramovich.alex@outlook.com>
2026-02-17 15:17:38 +0300

    test

--- modules/yc/client_config/main.tf
+++ modules/yc/client_config/main.tf
@@ -1,1 +1,14 @@
+variable "cloud_id" { type = optional(string) }
+variable "folder_id" { type = optional(string) }
+variable "zone" { type = optional(string) }
+variable "sa_key_file" { type = optional(string) }
+
+providers "yandex" {
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+  zone      = var.zone
+
+  service_account_key_file = var.sa_key_file
+}
+
 data "yandex_client_config" "client" {}
