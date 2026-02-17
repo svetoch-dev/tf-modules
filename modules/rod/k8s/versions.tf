4862d213aa8e6b873dde2113de573ea246771f34
Gramovich Alexander <gramovich.alex@outlook.com>
2026-02-17 10:28:01 +0300

    rod/k8s: add yandex cloud to versions

--- modules/rod/k8s/versions.tf
+++ modules/rod/k8s/versions.tf
@@ -1,5 +1,11 @@
 terraform {
   required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
     deepmerge = {
       source  = "isometry/deepmerge"
       version = "1.2.1"
