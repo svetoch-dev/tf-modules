4657c0a3ada93cb1cc33861536919d2e1a1d7e35
Gramovich Alexander <gramovich.alex@outlook.com>
2026-02-21 12:48:50 +0300

    rod/cloud/gcp: remove none needed providers

--- modules/rod/cloud/gcp/versions.tf
+++ modules/rod/cloud/gcp/versions.tf
@@ -1,17 +1,5 @@
 terraform {
   required_providers {
-    google = {
-      source = "hashicorp/google"
-    }
-    google-beta = {
-      source = "hashicorp/google-beta"
-    }
-    random = {
-      source = "hashicorp/random"
-    }
-    null = {
-      source = "hashicorp/null"
-    }
     deepmerge = {
       source  = "isometry/deepmerge"
       version = "1.2.1"
