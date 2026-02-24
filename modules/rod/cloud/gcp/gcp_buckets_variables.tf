82a951b0b8b2fdfba9cf1cd3b88c4c774291aedf
Gramovich Alexander <gramovich.alex@outlook.com>
2026-02-24 17:56:31 +0300

    soft_delete_duration for logs/runners-cache buckets

--- modules/rod/cloud/gcp/gcp_buckets_variables.tf
+++ modules/rod/cloud/gcp/gcp_buckets_variables.tf
@@ -50,10 +50,11 @@ locals {
     }
     format("%s-logs-%s", var.company.name, var.env.short_name) = {
       #force_destroy should be oposite to deletion_protection 
-      force_destroy      = var.env.cloud.buckets.deletion_protection ? false : true
-      storage_class      = "STANDARD"
-      location           = var.env.cloud.location.region
-      bucket_policy_only = true
+      force_destroy        = var.env.cloud.buckets.deletion_protection ? false : true
+      storage_class        = "STANDARD"
+      location             = var.env.cloud.location.region
+      bucket_policy_only   = true
+      soft_delete_duration = 0
       admins = [
         "serviceAccount:fluent@${var.env.cloud.id}.iam.gserviceaccount.com"
       ]
@@ -73,9 +74,10 @@ locals {
     }
     format("%s-runners-cache-%s", var.company.name, var.env.short_name) = var.env.short_name == "int" ? {
       #force_destroy should be oposite to deletion_protection 
-      force_destroy = var.env.cloud.buckets.deletion_protection ? false : true
-      storage_class = "STANDARD"
-      location      = var.env.cloud.location.region
+      force_destroy        = var.env.cloud.buckets.deletion_protection ? false : true
+      storage_class        = "STANDARD"
+      location             = var.env.cloud.location.region
+      soft_delete_duration = 0
       admins = [
         "serviceAccount:runner-app@${var.env.cloud.id}.iam.gserviceaccount.com"
       ]
