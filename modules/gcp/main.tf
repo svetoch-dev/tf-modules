3d0eb78302afa9e272c7ec677072913d18111937
Ruslan Palikarpau <r.palikarpau@trykitchenhub.com>
2026-04-07 14:29:44 +0300

    test

--- modules/gcp/main.tf
+++ modules/gcp/main.tf
@@ -69,21 +69,23 @@ module "gke" {
   #   if cluster_obj != null && try(cluster_obj.cluster.enabled, true) == true
   # ]...)
 
-  node_pools = merge([
-  for cluster_name, cluster_obj in var.gke : {
-    for pool_name, pool_obj in try(cluster_obj.node_pools, {}) :
-    "${cluster_name}/${pool_name}" => merge(
-      { name = pool_name },
-      pool_obj,
-      {
-        cluster  = cluster_name
-        location = cluster_obj.location
-        node_locations = cluster_obj.node_locations
-      }
-    )
-  }
-  if cluster_obj != null && try(cluster_obj.enabled, true)
-  ]...)
+  # node_pools = merge([
+  # for cluster_name, cluster_obj in var.gke : {
+  #   for pool_name, pool_obj in cluster_obj.node_pools :
+  #   "${cluster_name}/${pool_name}" => merge(
+  #     { name = pool_name },
+  #     pool_obj,
+  #     {
+  #       cluster  = cluster_name
+  #       location = cluster_obj.location
+  #       node_locations = cluster_obj.node_locations
+  #     }
+  #   )
+  # }
+  # if cluster_obj != null && try(cluster_obj.enabled, true)
+  # ]...)
+
+  node_pools = {}
 
   depends_on = [
     module.enable_apis,
