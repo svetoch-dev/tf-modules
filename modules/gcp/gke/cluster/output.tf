30f8069bf7f3cad02d750a996bdeb2c94cea0b6a
Ruslan Palikarpau <r.palikarpau@trykitchenhub.com>
2026-04-09 20:35:32 +0300

    fix this in outputs

--- modules/gcp/gke/cluster/output.tf
+++ modules/gcp/gke/cluster/output.tf
@@ -1,23 +1,23 @@
 output "name" {
-  value = google_container_cluster.cluster.name
+  value = google_container_cluster.this.name
 }
 
 output "location" {
-  value = google_container_cluster.cluster.location
+  value = google_container_cluster.this.location
 }
 
 output "endpoint" {
-  value = google_container_cluster.cluster.endpoint
+  value = google_container_cluster.this.endpoint
 }
 
 output "self_link" {
-  value = google_container_cluster.cluster.self_link
+  value = google_container_cluster.this.self_link
 }
 
 output "master_version" {
-  value = google_container_cluster.cluster.master_version
+  value = google_container_cluster.this.master_version
 }
 
 output "id" {
-  value = google_container_cluster.cluster.id
+  value = google_container_cluster.this.id
 }
--- modules/gcp/gke/node_pool/output.tf
+++ modules/gcp/gke/node_pool/output.tf
@@ -1,15 +1,15 @@
 output "name" {
-  value = google_container_node_pool.node_pool.name
+  value = google_container_node_pool.this.name
 }
 
 output "id" {
-  value = google_container_node_pool.node_pool.id
+  value = google_container_node_pool.this.id
 }
 
 output "managed_instance_group_urls" {
-  value = google_container_node_pool.node_pool.managed_instance_group_urls
+  value = google_container_node_pool.this.managed_instance_group_urls
 }
 
 output "instance_group_urls" {
-  value = google_container_node_pool.node_pool.instance_group_urls
+  value = google_container_node_pool.this.instance_group_urls
 }
