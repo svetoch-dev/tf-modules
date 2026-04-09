6e7c1214d39f94dac2054a196d0090a2d32364de
Ruslan Palikarpau <r.palikarpau@trykitchenhub.com>
2026-04-09 10:24:52 +0300

    comment network

--- modules/rod/cloud/gcp/gcp_gke_clusters_variables.tf
+++ modules/rod/cloud/gcp/gcp_gke_clusters_variables.tf
@@ -6,9 +6,9 @@ locals {
       deletion_protection = var.env.kubernetes.deletion_protection
       location            = var.env.kubernetes.regional ? var.env.cloud.location.region : var.env.kubernetes.node_locations[0]
       node_locations      = var.env.kubernetes.node_locations
-
+      #"projects/${var.env.cloud.id}/regions/${var.env.cloud.region}/subnetworks/${module.gcp.subnets["main"]["vms"]}"
       network                 = "projects/${var.env.cloud.id}/global/networks/${module.gcp.vpcs["main"]}"
-      subnetwork              = "projects/${var.env.cloud.id}/regions/${var.env.cloud.region}/subnetworks/${module.gcp.subnets["main"]["vms"]}"
+      subnetwork              = module.gcp.subnets["main"]["vms"].self_link
       ip_range_pods           = module.gcp.subnets["main"]["vms"].secondary_ip_range[0].range_name
       ip_range_services       = module.gcp.subnets["main"]["vms"].secondary_ip_range[1].range_name
       network_policy          = {
