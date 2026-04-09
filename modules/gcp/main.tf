d82e275c6aad1cdfac594b396cde40670f02fb20
Ruslan Palikarpau <r.palikarpau@trykitchenhub.com>
2026-04-09 20:46:12 +0300

    formatting

--- modules/gcp/main.tf
+++ modules/gcp/main.tf
@@ -48,19 +48,21 @@ module "gke" {
     if cluster_obj.enabled
   }
 
-  node_pools = merge([
-    for cluster_name, cluster_obj in var.gke : {
-      for pool_name, pool_obj in cluster_obj.node_pools :
-      "${cluster_name}/${pool_name}" => merge(
-        {
-          location = cluster_obj.location
-          cluster  = cluster_name
-        },
-        pool_obj
-      )
-    }
-    if cluster_obj != null && try(cluster_obj.enabled, true)
-  ]...)
+  node_pools = merge(
+    [
+      for cluster_name, cluster_obj in var.gke : {
+        for pool_name, pool_obj in cluster_obj.node_pools :
+        "${cluster_name}/${pool_name}" => merge(
+          {
+            location = cluster_obj.location
+            cluster  = cluster_name
+          },
+          pool_obj
+        )
+      }
+      if cluster_obj != null && try(cluster_obj.enabled, true)
+    ]
+  ...)
 
   depends_on = [
     module.enable_apis,
