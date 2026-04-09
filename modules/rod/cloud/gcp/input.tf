64b9885b7003b443b8a3ece10f04dc0dc320f3a4
Ruslan Palikarpau <r.palikarpau@trykitchenhub.com>
2026-04-09 21:04:32 +0300

    mv name gcp_gke_clusters to gcp_k8s_clusters

--- modules/rod/cloud/gcp/input.tf
+++ modules/rod/cloud/gcp/input.tf
@@ -107,7 +107,7 @@ variable "overrides" {
       gcp_buckets       = optional(any)
       gcp_dns_zones     = optional(any)
       gcp_iam           = optional(any)
-      gcp_gke_clusters  = optional(any)
+      gcp_k8s_clusters  = optional(any)
       gcp_logging       = optional(any)
       gcp_networks      = optional(any)
       gcp_registries    = optional(any)
@@ -118,7 +118,7 @@ variable "overrides" {
     gcp_buckets       = null
     gcp_dns_zones     = null
     gcp_iam           = null
-    gcp_gke_clusters  = null
+    gcp_k8s_clusters  = null
     gcp_logging       = null
     gcp_networks      = null
     gcp_registries    = null
--- modules/rod/cloud/gcp/overrides.tf
+++ modules/rod/cloud/gcp/overrides.tf
@@ -3,7 +3,7 @@ locals {
   gcp_buckets_merged       = provider::deepmerge::mergo(local.gcp_buckets, var.overrides.gcp_buckets)
   gcp_dns_zones_merged     = provider::deepmerge::mergo(local.gcp_dns_zones, var.overrides.gcp_dns_zones)
   gcp_iam_merged           = provider::deepmerge::mergo(local.gcp_iam, var.overrides.gcp_iam)
-  gcp_gke_clusters_merged  = provider::deepmerge::mergo(local.gcp_gke_clusters, var.overrides.gcp_gke_clusters)
+  gcp_k8s_clusters_merged  = provider::deepmerge::mergo(local.gcp_k8s_clusters, var.overrides.gcp_k8s_clusters)
   gcp_logging_merged       = provider::deepmerge::mergo(local.gcp_logging, var.overrides.gcp_logging)
   gcp_networks_merged      = provider::deepmerge::mergo(local.gcp_networks, var.overrides.gcp_networks)
   gcp_registries_merged    = provider::deepmerge::mergo(local.gcp_registries, var.overrides.gcp_registries)
