fc5e2ce816c0cbfb605e352da37a3b69649d00f6
Ruslan Palikarpau <r.palikarpau@trykitchenhub.com>
2026-04-09 20:24:25 +0300

    formatting node_pool inputs

--- modules/gcp/gke/node_pool/input.tf
+++ modules/gcp/gke/node_pool/input.tf
@@ -44,99 +44,142 @@ variable "max_pods_per_node" {
 
 variable "autoscaling" {
   description = "Configuration for node pool autoscaling"
-  type = object({
-    min_node_count       = optional(number, 0)
-    max_node_count       = optional(number, 1)
-    total_min_node_count = optional(number, 0)
-    total_max_node_count = optional(number, 0)
-    location_policy      = optional(string, "ANY")
-  })
+  type = object(
+    {
+      min_node_count       = optional(number, 0)
+      max_node_count       = optional(number, 1)
+      total_min_node_count = optional(number, 0)
+      total_max_node_count = optional(number, 0)
+      location_policy      = optional(string, "ANY")
+    }
+  )
   default = {}
 }
 
 variable "management" {
   description = "Configuration for node pool management"
-  type = object({
-    auto_repair  = optional(bool, true)
-    auto_upgrade = optional(bool, true)
-  })
-  default = {
-    auto_repair  = true
-    auto_upgrade = true
-  }
+  type = object(
+    {
+      auto_repair  = optional(bool, true)
+      auto_upgrade = optional(bool, true)
+    }
+  )
+  default = {}
 }
 
 variable "node_config" {
   description = "Configuration for node pool nodes"
-  type = object({
-    machine_type    = optional(string, "e2-medium")
-    service_account = optional(string, "default")
-    oauth_scopes = optional(list(string),
-      [
-        "https://www.googleapis.com/auth/userinfo.email",
-        "https://www.googleapis.com/auth/cloud-platform"
-      ]
-    )
-    disk_size_gb = optional(number, 100)
-    disk_type    = optional(string, "pd-ssd")
-    image_type   = optional(string, "COS_CONTAINERD")
-    labels       = optional(map(string), {})
-    metadata = optional(map(string), {
-      "disable-legacy-endpoints" = "true"
-    })
-    tags            = optional(list(string), [])
-    preemptible     = optional(bool, false)
-    spot            = optional(bool, false)
-    local_ssd_count = optional(number, 0)
-    taint = optional(list(object({
-      key    = string
-      value  = string
-      effect = string
-    })), [])
-    workload_metadata_config = optional(object({
-      mode = optional(string, "GKE_METADATA")
-    }), {})
-    shielded_instance_config = optional(object({
-      enable_secure_boot          = optional(bool, false)
-      enable_integrity_monitoring = optional(bool, true)
-    }), {})
-    kubelet_config = optional(object({
-      allowed_unsafe_sysctls                 = optional(list(string), [])
-      container_log_max_files                = optional(number, 0)
-      image_gc_high_threshold_percent        = optional(number, 0)
-      image_gc_low_threshold_percent         = optional(number, 0)
-      insecure_kubelet_readonly_port_enabled = optional(string, "FALSE")
-      cpu_cfs_quota                          = optional(bool, false)
-      pod_pids_limit                         = optional(number, 0)
-    }), {})
-    linux_node_config = optional(object({
-      sysctls     = optional(map(string))
-      cgroup_mode = optional(string)
-    }))
-    gvnic = optional(object({
-      enabled = bool
-    }))
-    reservation_affinity = optional(object({
-      consume_reservation_type = string
-      key                      = optional(string)
-      values                   = optional(list(string))
-    }))
-    enable_confidential_storage = optional(bool, false)
-    flex_start                  = optional(bool, false)
-    logging_variant             = optional(string, "DEFAULT")
-    resource_manager_tags       = optional(map(string), {})
-    storage_pools               = optional(list(string), [])
-  })
+  type = object(
+    {
+      machine_type    = optional(string, "e2-medium")
+      service_account = optional(string, "default")
+      oauth_scopes = optional(
+        list(string),
+        [
+          "https://www.googleapis.com/auth/userinfo.email",
+          "https://www.googleapis.com/auth/cloud-platform"
+        ]
+      )
+      disk_size_gb = optional(number, 100)
+      disk_type    = optional(string, "pd-ssd")
+      image_type   = optional(string, "COS_CONTAINERD")
+      labels       = optional(map(string), {})
+      metadata = optional(
+        map(string),
+        {
+          "disable-legacy-endpoints" = "true"
+        }
+      )
+      tags            = optional(list(string), [])
+      preemptible     = optional(bool, false)
+      spot            = optional(bool, false)
+      local_ssd_count = optional(number, 0)
+      taint = optional(
+        list(
+          object(
+            {
+              key    = string
+              value  = string
+              effect = string
+            }
+          )
+        ),
+        []
+      )
+      workload_metadata_config = optional(
+        object(
+          {
+            mode = optional(string, "GKE_METADATA")
+          }
+        ),
+        {}
+      )
+      shielded_instance_config = optional(
+        object(
+          {
+            enable_secure_boot          = optional(bool, false)
+            enable_integrity_monitoring = optional(bool, true)
+          }
+        ),
+        {}
+      )
+      kubelet_config = optional(
+        object(
+          {
+            allowed_unsafe_sysctls                 = optional(list(string), [])
+            container_log_max_files                = optional(number, 0)
+            image_gc_high_threshold_percent        = optional(number, 0)
+            image_gc_low_threshold_percent         = optional(number, 0)
+            insecure_kubelet_readonly_port_enabled = optional(string, "FALSE")
+            cpu_cfs_quota                          = optional(bool, false)
+            pod_pids_limit                         = optional(number, 0)
+          }
+        ),
+        {}
+      )
+      linux_node_config = optional(
+        object(
+          {
+            sysctls     = optional(map(string))
+            cgroup_mode = optional(string)
+          }
+        )
+      )
+      gvnic = optional(
+        object(
+          {
+            enabled = bool
+          }
+        )
+      )
+      reservation_affinity = optional(
+        object(
+          {
+            consume_reservation_type = string
+            key                      = optional(string)
+            values                   = optional(list(string))
+          }
+        )
+      )
+      enable_confidential_storage = optional(bool, false)
+      flex_start                  = optional(bool, false)
+      logging_variant             = optional(string, "DEFAULT")
+      resource_manager_tags       = optional(map(string), {})
+      storage_pools               = optional(list(string), [])
+    }
+  )
   default = {}
 }
 
 variable "upgrade_settings" {
   description = "Configuration for node pool upgrade settings"
-  type = object({
-    max_surge       = number
-    max_unavailable = number
-    strategy        = optional(string)
-  })
+  type = object(
+    {
+      max_surge       = number
+      max_unavailable = number
+      strategy        = optional(string)
+    }
+  )
   default = {
     max_surge       = 1
     max_unavailable = 0
@@ -145,37 +188,45 @@ variable "upgrade_settings" {
 
 variable "placement_policy" {
   description = "Configuration for node placement policy"
-  type = object({
-    type = string
-  })
+  type = object(
+    {
+      type = string
+    }
+  )
   default = null
 }
 
 variable "queued_provisioning" {
   description = "Configuration for queued provisioning"
-  type = object({
-    enabled = bool
-  })
+  type = object(
+    {
+      enabled = bool
+    }
+  )
   default = null
 }
 
 variable "network_config" {
   description = "Configuration for node pool networking"
-  type = object({
-    create_pod_range     = optional(bool, false)
-    pod_range            = optional(string)
-    pod_ipv4_cidr_block  = optional(string)
-    enable_private_nodes = optional(bool, true)
-  })
+  type = object(
+    {
+      create_pod_range     = optional(bool, false)
+      pod_range            = optional(string)
+      pod_ipv4_cidr_block  = optional(string)
+      enable_private_nodes = optional(bool, true)
+    }
+  )
   default = {}
 }
 
 variable "timeouts" {
   description = "This resource provides the following Timeouts configuration options"
-  type = object({
-    create = optional(string, "45m")
-    delete = optional(string, "45m")
-    update = optional(string, "45m")
-  })
+  type = object(
+    {
+      create = optional(string, "45m")
+      delete = optional(string, "45m")
+      update = optional(string, "45m")
+    }
+  )
   default = {}
 }
