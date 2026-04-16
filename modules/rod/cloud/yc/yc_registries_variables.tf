71b6d7bb32db976e55c649d2d583b3aea9a542e3
dmitry231187 <leshiy74@gmail.com>
2026-04-16 12:09:30 +0000

    delete readers

--- modules/rod/cloud/yc/yc_registries_variables.tf
+++ modules/rod/cloud/yc/yc_registries_variables.tf
@@ -1,9 +1,6 @@
 locals {
   yc_registries = {
     containers = {
-      readers = var.env.initial_start ? [] : [
-        "serviceAccount:${module.yc.iam.service_accounts["k8s-nodes"].id}"
-      ]
       writers = var.env.short_name != "int" && var.env.initial_start != true ? [
         "serviceAccount:${module.yc.iam.service_accounts["runner-app"].id}"
       ] : []
