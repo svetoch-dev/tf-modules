d368f9d18a5bd5f4776b3cddf4b8d29f6d9b9ae0
Gramovich Alexander <gramovich.alex@outlook.com>
2026-04-17 13:52:23 +0300

    fix runner-app sa names

--- modules/rod/cloud/yc/yc_registries_variables.tf
+++ modules/rod/cloud/yc/yc_registries_variables.tf
@@ -2,7 +2,7 @@ locals {
   yc_registries = {
     containers = {
       writer_names = var.env.short_name != "int" ? [
-        "serviceAccountName:${var.int_env.cloud.folder_id}:runner-${var.int_env.short_name}"
+        "serviceAccountName:${var.int_env.cloud.folder_id}:runner-app-${var.int_env.short_name}"
       ] : []
     }
   }
