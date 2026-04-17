1fd67fd1f597951952709ecbf4b170ad4e236800
Gramovich Alexander <gramovich.alex@outlook.com>
2026-04-17 12:45:02 +0300

    remove test

--- modules/rod/cloud/yc/yc_dns_zones_variables.tf
+++ modules/rod/cloud/yc/yc_dns_zones_variables.tf
@@ -3,15 +3,6 @@ locals {
     tostring(var.env.name) = {
       name = var.env.name
       zone = format("%s.%s.", var.env.short_name, var.company.domain)
-      records = [
-        {
-          name = "hui"
-          type = "A"
-          data = ["192.168.0.1"]
-
-          ttl = 200
-        }
-      ]
     }
   }
 }
