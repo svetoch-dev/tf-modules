305f77f94169e280648081820a5dd163f781e83c
dmitry231187 <leshiy74@gmail.com>
2026-05-06 07:16:27 +0000

    create unique names of variables and secrets

--- modules/rod/ci/gha/input.tf
+++ modules/rod/ci/gha/input.tf
@@ -19,31 +19,36 @@ variable "ci_vars" {
   type = list(
     object(
       {
-        ci_name    = string
-        repo_name  = string
-        repo_group = string
-        cd_branch  = optional(string)
-        cd_file    = optional(string)
-        cd_path    = optional(string)
-        vars = optional(
-          list(
-            object(
-              {
-                name  = string
-                value = string
-              }
+        ci_name        = string
+        env_short_name = string
+        ci_data = object(
+          {
+            repo_name  = string
+            repo_group = string
+            cd_branch  = optional(string)
+            cd_file    = optional(string)
+            cd_path    = optional(string)
+            vars = optional(
+              list(
+                object(
+                  {
+                    name  = string
+                    value = string
+                  }
+                )
+              ), []
             )
-          ), []
-        )
-        secrets = optional(
-          list(
-            object(
-              {
-                name  = string
-                value = string
-              }
+            secrets = optional(
+              list(
+                object(
+                  {
+                    name  = string
+                    value = string
+                  }
+                )
+              ), []
             )
-          ), []
+          }
         )
       }
     )
--- modules/rod/ci/gl/ci_variables.tf
+++ modules/rod/ci/gl/ci_variables.tf
@@ -14,30 +14,30 @@ locals {
     },
     {
       for ci_obj in var.ci_vars : ci_obj.ci_name => {
-        name = ci_obj.repo_name
-        org  = ci_obj.repo_group
+        name = ci_obj.ci_data.repo_name
+        org  = ci_obj.ci_data.repo_group
         vars = merge(
           {
             for k, v in {
-              cd_branch = ci_obj.cd_branch
-              cd_file   = ci_obj.cd_file
-              cd_path   = ci_obj.cd_path
+              cd_branch = ci_obj.ci_data.cd_branch
+              cd_file   = ci_obj.ci_data.cd_file
+              cd_path   = ci_obj.ci_data.cd_path
               } : k => {
-              name  = upper(k)
+              name  = "${upper(k)}_${upper(ci_obj.env_short_name)}"
               value = v
             }
             if v != null && v != ""
           },
           {
-            for var in ci_obj.vars : var.name => {
-              name  = var.name
+            for var in ci_obj.ci_data.vars : var.name => {
+              name  = "${upper(var.name)}_${upper(ci_obj.env_short_name)}"
               value = var.value
             }
           }
         )
         secrets = {
-          for secret in ci_obj.secrets : secret.name => {
-            name       = secret.name
+          for secret in ci_obj.ci_data.secrets : secret.name => {
+            name       = "${upper(secret.name)}_${upper(ci_obj.env_short_name)}"
             text_value = secret.value
           }
         }
