9ed893821567827f5022459cebe223ef70c24f65
dmitry231187 <leshiy74@gmail.com>
2026-05-07 18:55:05 +0000

    fix repo group

--- modules/rod/ci/gha/ci_configs.tf
+++ modules/rod/ci/gha/ci_configs.tf
@@ -15,14 +15,14 @@ locals {
     {
       for ci_obj in var.ci_configs : ci_obj.ci_name => {
         name = ci_obj.repo.name
-        org  = ci_obj.repo.group != "" ? ci_obj.repo.group : var.repo.group
+        org  = try(ci_obj.repo.group, var.repo.group)
         vars = (
           {
             for k, v in {
               registry_url = ci_obj.env_registry_url
               cd_branch    = ci_obj.cd.branch
               cd_file      = ci_obj.cd.file
-              cd_path      = ci_obj.cd.tag_path
+              cd_tag_path  = ci_obj.cd.tag_path
               } : k => {
               name  = replace(upper("${k}_${ci_obj.env_short_name}"), "-", "_")
               value = v
