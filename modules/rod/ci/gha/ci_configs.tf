99a41204f253b50be3d82514a3651bb1170f54ab
dmitry231187 <leshiy74@gmail.com>
2026-05-12 09:49:24 +0000

    refactor names

--- modules/rod/ci/gha/ci_configs.tf
+++ modules/rod/ci/gha/ci_configs.tf
@@ -13,19 +13,19 @@ locals {
       }
     },
     {
-      for ci_obj in var.ci_configs :
-      "${ci_obj.env_short_name}_${ci_obj.ci_name}" => {
+      for ci_obj in var.app_env_ci_configs :
+      "${ci_obj.env.short_name}_${ci_obj.app_name}" => {
         name = ci_obj.repo.name
         org  = coalesce(ci_obj.repo.group, var.repo.group)
         vars = (
           {
             for var_name, var_obj in {
-              registry_url = ci_obj.env_registry_url
+              registry_url = ci_obj.env.registry_url
               cd_branch    = ci_obj.cd.branch
               cd_file      = ci_obj.cd.file
               cd_tag_path  = ci_obj.cd.tag_path
               } : var_name => {
-              name  = replace(upper("${ci_obj.env_short_name}_${var_name}"), "-", "_")
+              name  = replace(upper("${ci_obj.env.short_name}_${var_name}"), "-", "_")
               value = var_obj
             }
             if var_obj != null && var_obj != ""
