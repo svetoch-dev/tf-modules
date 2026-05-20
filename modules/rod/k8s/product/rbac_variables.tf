f071ee9a2221b79f66d22dfc4121b478960444c9
Gramovich Alexander <gramovich.alex@outlook.com>
2026-05-20 17:14:40 +0300

    use lookup

--- modules/rod/k8s/product/rbac_variables.tf
+++ modules/rod/k8s/product/rbac_variables.tf
@@ -1,22 +1,28 @@
 locals {
   rbac = {
     cluster_role_binding = {
-      argocd = var.env.cloud.name == "gcp" ? {
-        labels      = {},
-        annotations = {},
-        role_ref = {
-          kind = "ClusterRole"
-          name = "cluster-admin"
-        }
-        subject = {
-          argocd = {
-            api_group = "rbac.authorization.k8s.io"
-            kind      = "User"
-            name      = "argocd@${var.int_env.cloud.id}.iam.gserviceaccount.com"
-            namespace = ""
+      argocd = lookup(
+        {
+          gcp = {
+            labels      = {},
+            annotations = {},
+            role_ref = {
+              kind = "ClusterRole"
+              name = "cluster-admin"
+            }
+            subject = {
+              argocd = {
+                api_group = "rbac.authorization.k8s.io"
+                kind      = "User"
+                name      = "argocd@${var.int_env.cloud.id}.iam.gserviceaccount.com"
+                namespace = ""
+              }
+            }
           }
-        }
-      } : null
+        },
+        var.env.cloud.name,
+        null
+      )
     }
   }
 }
