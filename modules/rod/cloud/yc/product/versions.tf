2ac8e62bc53e071ddbf07e9bf83ca6064206cc12
Gramovich Alexander <gramovich.alex@outlook.com>
2026-05-22 12:15:42 +0300

    use symlink

--- modules/rod/cloud/yc/product/versions.tf
+++ modules/rod/cloud/yc/product/versions.tf
@@ -1,12 +1,1 @@
-terraform {
-  required_providers {
-    yandex = {
-      source = "yandex-cloud/yandex"
-    }
-    deepmerge = {
-      source  = "isometry/deepmerge"
-      version = "1.2.1"
-    }
-  }
-  required_version = ">= 1.8"
-}
+../versions.tf
