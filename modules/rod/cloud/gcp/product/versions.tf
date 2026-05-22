b8abb03ae4b789d416a38b150bda15f62547a829
Gramovich Alexander <gramovich.alex@outlook.com>
2026-05-22 13:52:57 +0300

    gcp use symlink for version

--- modules/rod/cloud/gcp/product/versions.tf
+++ modules/rod/cloud/gcp/product/versions.tf
@@ -1,14 +1,1 @@
-terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-    }
-    google-beta = {
-      source = "hashicorp/google-beta"
-    }
-    deepmerge = {
-      source = "isometry/deepmerge"
-    }
-  }
-  required_version = ">= 1.8"
-}
+../versions.tf
